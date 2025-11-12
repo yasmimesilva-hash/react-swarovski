c442c3ec614c3e6b7f8c86006a6a79fa3b4cb7cd
YASMIM EUGÊNIO DA SILVA <45259349806@info.local>
2025-11-12 13:43:43 -0300

    inicio do projeto

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Produtos from "./pages/Produtos/Produtos"
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/produtos" element={<Produtos/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
